6efb6cfeee03acd91560a04ebd4a933190c957dc
inoy <7m7110123@gmail.com>
2021-06-16 20:55:14 +0900

    複数の状態を管理しよう

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const reset = () => {
+    setName(props.name);
+    setPrice(props.price);
+  };
 
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-
-  /* count表示を囲むdivタグにはid={Date.now()}を設定。setCountによりcountが更新されたとき再描画されていることを確認。
-     3の倍数のときだけ3で割るで、3の倍数でないとき（setCount(count)のとき）はidが変わらないことを確認。 */
   return (
     <>
-      <div id={Date.now()}>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={() => setCount(0)}>Reset</button>
-      </div>
-      <div>
-        <button onClick={() => setCount(count * 2)}>*2</button>
-        <button onClick={() => setCount(count / 2)}>/2</button>
-      </div>
-      <div>
-        <button
-          onClick={() =>
-            count % 3 === 0 ? setCount(count / 3) : setCount(count)
-          }
-        >
-          3の倍数のときだけ3で割る
-        </button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: 'サンプル',
+  price: 1000,
 };
 
 export default App;
