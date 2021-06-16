34d9fb39f50b78ac592a0e87a13220f22d16fccc
inoy <7m7110123@gmail.com>
2021-06-16 22:28:03 +0900

    イベントの状態遷移を管理するreducerを作成しよう

--- README.md
+++ README.md
@@ -88,3 +88,15 @@ useEffect(() => {
   console.log('This callback is for name only.');
 }, [name]);
 ```
+
+## [useReducer](https://ja.reactjs.org/docs/hooks-reference.html#usereducer)
+
+### TODO あとで読む
+
+learn/lecture/15158680#questions/8280808
+
+> そこで登場するのがカスタムフックです。
+> https://reactjs.org/docs/hooks-custom.html
+
+> 1. 外部からデータを取得する処理をコンポーネント内部に実装した例 issues/8280808/before というブランチをご確認ください。
+> 2. 1 で実装した処理をカスタムフックに分離し、そのカスタムフックを利用した例 issues/8280808/after というブランチをご確認ください。
---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: 'タイトル1',
+//   body: 'ボディー1',
+// };
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1' },
+//   { id: 2, title: 'タイトル2', body: 'ボディー2' },
+// ];
+const newId = (state) => {
+  const length = state.length;
+  if (length === 0) {
+    return 1;
+  } else {
+    return state[length - 1].id++;
+  }
+};
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body };
+      return [...state, { ...event, id: newId(state) }];
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENTS':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
